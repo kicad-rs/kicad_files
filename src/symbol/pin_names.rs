8e3a7ccc4ba6a5440354c5c889d26aeeb94e2c05
Dominic <git@msrd0.de>
2022-01-26 09:04:18 +0100

    use new monostate lib

--- src/symbol/pin_names.rs
+++ src/symbol/pin_names.rs
@@ -1,4 +1,5 @@
 use crate::{internal::option_tuple, mm};
+use monostate::MustBe;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
@@ -7,7 +8,16 @@ pub struct PinNames {
 	#[serde(with = "option_tuple")]
 	pub offset: Option<mm>,
 
-	pub hide: bool
+	hide: MustBe!(true)
+}
+
+impl PinNames {
+	pub const fn new(offset: Option<mm>) -> Self {
+		Self {
+			offset,
+			hide: MustBe!(true)
+		}
+	}
 }
 
 #[cfg(test)]
@@ -18,18 +28,12 @@ mod tests {
 	sexpr_test_case! {
 		name: pin_names_no_offset,
 		input: r#"(pin_names hide)"#,
-		value: PinNames {
-			offset: None,
-			hide: true
-		}
+		value: PinNames::new(None)
 	}
 
 	sexpr_test_case! {
 		name: pin_names_with_offset,
 		input: r#"(pin_names (offset 0.508) hide)"#,
-		value: PinNames {
-			offset: Some(0.508.mm()),
-			hide: true
-		}
+		value: PinNames::new(Some(0.508.mm()))
 	}
 }
